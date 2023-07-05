7d9b850836c7bcadb852d88492908077f9228d2e
ristiatmoko <ristiatmoko@gmail.com>
2023-07-06 00:02:32 +0700

    Finishing

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('dashboard.users.index', [
+            'users' => User::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('dashboard.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:3|max:255|',
+
+        ]);
+
+//        $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+//        $request->session()->flash('success', 'Registration successful! Please Login');
+
+        return redirect('/dashboard/user')->with('success', 'Registrasi berhasil!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        return view('dashboard.users.edit', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email:dns',
+            'password' => 'required|min:3|max:255|',
+
+        ]);
+
+//        $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::where('id', $user->id)
+            ->update($validatedData);
+//        $request->session()->flash('success', 'Registration successful! Please Login');
+
+        return redirect('/dashboard/user')->with('success', 'Admin baru berhasil diperbarui!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        User::destroy($user->id);
+        return redirect('/dashboard/user')->with('success', 'Admin baru berhasil dihapus!');
+    }
+}
---
+++ resources/views/dashboard/laporan/create.blade.php
@@ -0,0 +1,169 @@
+@extends('dashboard.layouts.main')
+
+@section('content')
+
+    <div class="wrapper">
+
+{{--        @include('dashboard.partials.navbar')--}}
+
+        @include('dashboard.partials.sidebar')
+
+
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Tambah Obat</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">DataTables</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <!-- SELECT2 EXAMPLE -->
+                    <div class="card card-default">
+                        <div class="card-header">
+{{--                            <h3 class="card-title">Select2 (Default Theme)</h3>--}}
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+{{--                                <button type="button" class="btn btn-tool" data-card-widget="remove">--}}
+{{--                                    <i class="fas fa-times"></i>--}}
+{{--                                </button>--}}
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <form action="/dashboard/product" method="post">
+                            @csrf
+                            <div class="row">
+                                <div class="col-md-6">
+{{--                                    <div class="form-group">--}}
+{{--                                        <label>Kode Obat</label>--}}
+{{--                                        <input type="password" disabled="disabled"  class="form-control @error('username') is-invalid @enderror" id="password" placeholder="Password" name="password" required>--}}
+{{--                                    </div>--}}
+                                    <div class="form-group">
+                                        <label for="product_name">Nama Obat</label>
+                                        <input type="text" class="form-control @error('product_name') is-invalid @enderror" id="product_name" placeholder="Nama Obat" name="product_name" value="{{ old('product_name') }}">
+                                        @error('product_name')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="product_slug">Slug</label>
+                                        <input type="text" class="form-control @error('product_slug') is-invalid @enderror" id="product_slug" placeholder="Slug" name="product_slug" value="{{ old('product_slug') }}"  readonly>
+                                        @error('product_slug')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="category">Kategori Obat</label>
+                                        <select class="form-control select2" style="width: 100%;" name="category_id">
+                                            @foreach($categories as $category)
+                                                @if(old('category_id') == $category->id)
+                                                    <option value="{{ $category->id }}" selected>{{ $category->category_name }}</option>
+                                                @else
+                                                    <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                                @endif
+                                            @endforeach
+                                        </select>
+                                    </div>
+
+                                </div>
+                                <!-- /.col -->
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="product_stock">Stok Obat</label>
+                                        <input type="number" class="form-control @error('product_stock') is-invalid @enderror" id="product_stock" placeholder="Stock" name="product_stock" value="{{ old('product_stock') }}">
+                                        @error('product_stock')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="expired_date">Tanggal Kedaluwarsa </label>
+                                        <input type="date" class="form-control @error('expired_date') is-invalid @enderror" id="expired_date" placeholder="Stock" name="expired_date" value="{{ old('expired_date') }}">
+                                        @error('expired_date')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <!-- /.col -->
+                            </div>
+                            <!-- /.row -->
+                            <div class="row">
+                                <div class="col-12 col-sm-6">
+                                    <button type="submit" class="btn btn-primary">Tambah</button>
+                                </div>
+                            </div>
+                            <!-- /.row -->
+                            </form>
+
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            Visit <a href="https://select2.github.io/">Select2 documentation</a> for more examples and information about
+                            the plugin.
+                        </div>
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <script>
+            // $(document).ready(function () {
+            //     $('#myTable').DataTable();
+            // });
+            //
+            const product_name = document.querySelector('#product_name');
+            const product_slug = document.querySelector('#product_slug');
+
+            product_name.addEventListener('change', function () {
+                fetch('/dashboard/product/checkSlug?product_name=' + product_name.value)
+                    .then(response => response.json())
+                    .then(data => product_slug.value = data.product_slug)
+            });
+
+                {{--$('#product_name').change(function(e) {--}}
+                {{--    $.get('{{ url('check_slug') }}',--}}
+                {{--        { 'product_name': $(this).val() },--}}
+                {{--        function( data ) {--}}
+                {{--            $('#product_slug').val(data.product_slug);--}}
+                {{--        }--}}
+                {{--    );--}}
+                {{--});--}}
+
+        </script>
+
+
+        @include('partials.footer')
+    </div>
+    <!-- ./wrapper -->
+
+@endsection
+
+
+
