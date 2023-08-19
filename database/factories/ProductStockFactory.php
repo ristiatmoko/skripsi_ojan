c518f4d7d335f8c5b6c79b4f6c7c1e17fc507522
ristiatmoko <ristiatmoko@gmail.com>
2023-08-19 13:40:19 +0700

    Menambahkan fitur obat keluar & laporan obat keluar

---
+++ database/factories/ProductStockFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductStock>
+ */
+class ProductStockFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'product_id' => Product::factory(),
+            'amount' => $this->faker->randomDigit(1,3),
+            'description' => $this->faker->sentence(1,3),
+
+        ];
+    }
+}
